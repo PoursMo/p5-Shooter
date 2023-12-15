bb7aa421fb36a0b2b59cb5b13e89974b1977f582
Amaury Loubry <loubry.amaury@gmail.com>
2023-12-15 20:17:35 +0100

    Added function to slice sprite sheets

--- js/utilities.js
+++ js/utilities.js
@@ -233,3 +233,20 @@ class Damageable {
     }
   }
 }
+
+function sliceSpriteSheet(sheet, columns, rows){
+  let sprites = new Array()
+  let columnIndex = 0
+  let rowIndex=0
+  for(const index=0;index<rows*columns;index++){
+    sprites.push(sheet.get(sheet.width/columns*columnIndex,
+    sheet.height/rows*rowIndex,
+    sheet.width/columns,sheet.height/rows))
+  if(rowIndex===rowIndex) {
+    rowIndex=0
+    columnIndex++
+    }
+  else rowIndex++
+  }
+  return sprites
+}
